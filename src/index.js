5f9195a588a639549695e8016d0cdc536930aaf9
Michael McCormack <mcctwin@gmail.com>
2025-06-07 17:53:08 -0500

    1 test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.querySelector("#create-task-form").addEventListener("submit", (event) => {
+  event.preventDefault()
+  const taskDescription = document.querySelector("#new-task-description").value;
+  buildToDo(taskDescription);
+
 });
+
+function buildToDo(taskDescription){
+  const taskList = document.querySelector("#tasks");
+  const newTask = document.createElement("li");
+
+
+ 
+  const taskDesc = document.createElement("p");
+
+  
+  taskDesc.textContent = taskDescription;
+
+  newTask.append(taskDesc);
+  taskList.append(newTask);
+}
